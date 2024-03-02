52da8f29b66ebb5f862e18b6acf58cee67939ef5
Artem Bobylev <artematrr@gmail.com>
2024-03-02 21:24:45 +0400

    feature(CM-3): Сделал экспорт хэдера через файл index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import Header from '@/components/Header/Header';
+import Header from '@/components/Header'
 
 import './globals.css';
 
